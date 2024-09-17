c5e08caf8244060e68eeed90839296cdd740935d
atish <atish@DESKTOP-AIK0VK3>
2024-09-18 02:16:03 +0530

    My first commit

---
+++ src/com/p1/TestGit.java
@@ -0,0 +1,12 @@
+package com.p1;
+
+public class TestGit {
+	void m1(int a, int b) {
+		System.out.println(a+b);
+	}
+	public static void main(String[] args) {
+		TestGit tg = new TestGit();
+		tg.m1(10,20);
+	}
+
+}
